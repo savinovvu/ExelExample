9571fc0c2b8d2873f40596a801b2f092f9edc621
v.savinov <v.savinov@irkpk.ru>
2017-12-20 11:48:46 +0800

    set encoding.

--- src/main/java/ru/inbox/savinov_vu/ExcelReader.java
+++ src/main/java/ru/inbox/savinov_vu/ExcelReader.java
@@ -31,9 +31,9 @@ public class ExcelReader {
                 Cell cell = cellIterator.next();
 //                    CellType cellType = cell.getCellTypeEnum();
                 String nameAndDescription = cell.getStringCellValue();
-                String utf8String= new String(nameAndDescription.getBytes("CP866"), "CP1251");
+//                String utf8String= new String(nameAndDescription.getBytes("CP866"), "CP1251");
 
-                contractors.add(new Contractor(utf8String));
+                contractors.add(new Contractor(nameAndDescription));
             }
             System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
             System.out.println(contractors.size());
